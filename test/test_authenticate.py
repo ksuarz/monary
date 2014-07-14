557956f2785e9c90f1f370585e853022eeac530b
Matt Cotter <mcotter222@gmail.com>
2014-07-14 12:06:15 -0400

    Update heavily modified tests to pep8 compliance
    test_authenticate was almost entirely rewritten, and
    test_query_basics is new, so both are now updated to
    be pep8 compliant.

--- test/test_authenticate.py
+++ test/test_authenticate.py
@@ -2,39 +2,49 @@
 # Please see the included LICENSE.TXT and NOTICE.TXT for licensing information.
 
 import pymongo
+
 import monary
 
 db = None
 
+
 def setup():
     global db
-    assert '--auth' in pymongo.MongoClient().admin.command('getCmdLineOpts')['argv'], \
-           'The mongo server (mongod) needs to be running with authentication (--auth)'
+    cmd_opts = pymongo.MongoClient().admin.command('getCmdLineOpts')['argv']
+    assert "--auth" in cmd_opts, "The mongo server (mongod) needs to be"\
+                                 "running with authentication (--auth)"
     connection = pymongo.Connection()
     db = connection.monary_auth_test
     db.add_user("monary_test_user", "monary_test_pass")
     db.authenticate("monary_test_user", "monary_test_pass")
     db.junk.insert({"route": 66})
 
+
 def teardown():
     global db
     db.junk.drop()
     db.remove_user("monary_test_user")
+
 
 def test_with_authenticate():
     monary_connection = monary.Monary(host="127.0.0.1",
                                       database="monary_auth_test",
                                       username="monary_test_user",
                                       password="monary_test_pass")
-    [ col ] = monary_connection.query("monary_auth_test", "junk", {}, ["route"], ["int32"])
+    print 'c: %r' % monary_connection
+    col, = monary_connection.query("monary_auth_test", "junk",
+                                   {}, ["route"], ["int32"])
     print col
     assert col[0] == 66, "test value could not be retrieved"
 
+
 def test_with_authenticate_from_uri():
-    monary_connection = monary.Monary("mongodb://monary_test_user:monary_test_pass@"
-                                      "127.0.0.1:27017/monary_auth_test")
-    [ col ] = monary_connection.query("monary_auth_test", "junk", {}, ["route"], ["int32"])
+    monary_connection = monary.Monary("mongodb://monary_test_user:monary_test_"
+                                      "pass@127.0.0.1:27017/monary_auth_test")
+    col, = monary_connection.query("monary_auth_test", "junk",
+                                   {}, ["route"], ["int32"])
     assert col[0] == 66, "test value could not be retrieved"
+
 
 def test_bad_authenticate():
     try:
@@ -43,9 +53,11 @@ def test_bad_authenticate():
                                           username="monary_test_user",
                                           password="monary_test_wrong_pass")
         monary_connection.count("monary_auth_test", "junk", {})
-        assert False, "authentication should not have succeeded with wrong password"
+        assert False, "authentication should not have succeeded"\
+                      "with wrong password"
     except RuntimeError:
         pass  # auth should have failed
+
 
 def test_reconnection():
     try:
@@ -54,12 +66,14 @@ def test_reconnection():
                                           username="monary_test_user",
                                           password="monary_test_wrong_pass")
         monary_connection.count("monary_auth_test", "junk", {})
-        assert False, "authentication should not have succeeded with wrong password"
+        assert False, "authentication should not have succeeded"\
+                      "with wrong password"
     except RuntimeError:
         pass  # auth should have failed
     monary_connection.connect(host="127.0.0.1",
                               database="monary_auth_test",
                               username="monary_test_user",
                               password="monary_test_pass")
-    [ col ] = monary_connection.query("monary_auth_test", "junk", {}, ["route"], ["int32"])
+    col, = monary_connection.query("monary_auth_test", "junk",
+                                   {}, ["route"], ["int32"])
     assert col[0] == 66, "test value could not be retrieved"
--- test/test_query_basics.py
+++ test/test_query_basics.py
@@ -2,22 +2,26 @@
 # Please see the included LICENSE.TXT and NOTICE.TXT for licensing information.
 
 import pymongo
+
 import monary
 
 NUM_TEST_RECORDS = 5000
 
+
 def get_pymongo_connection():
     return pymongo.Connection("127.0.0.1")
+
 
 def get_monary_connection():
     return monary.Monary("127.0.0.1")
 
+
 def setup():
     with get_pymongo_connection() as c:
-        c.drop_database("monary_test") # ensure that database does not exist
+        c.drop_database("monary_test")  # ensure that database does not exist
         db = c.monary_test
         coll = db.test_data
-        records = [ ]
+        records = []
         for i in xrange(NUM_TEST_RECORDS):
             r = {"_id": i}
             if (i % 2) == 0:
@@ -26,23 +30,29 @@ def setup():
         coll.insert(records, safe=True)
         print "setup complete"
 
+
 def teardown():
     c = get_pymongo_connection()
     c.drop_database("monary_test")
     print "teardown complete"
 
+
 def get_monary_column(colname, coltype):
     with get_monary_connection() as m:
-        return m.query("monary_test", "test_data", {}, [colname], [coltype], sort="sequence")[0]
+        return m.query("monary_test", "test_data", {}, [colname],
+                       [coltype], sort="sequence")[0]
+
 
 def test_count():
     with get_monary_connection() as m:
         assert m.count("monary_test", "test_data", {}) == NUM_TEST_RECORDS
 
+
 def test_masks():
     vals = get_monary_column("x", "int8")
     target_records = int(NUM_TEST_RECORDS / 2)
     assert vals.count() == target_records
+
 
 def test_sum():
     vals = get_monary_column("_id", "int32")
