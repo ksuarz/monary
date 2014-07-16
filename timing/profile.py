abf6ddbc22cf7ed1b1ec44ac044e9591325d6b69
Matt Cotter <mcotter222@gmail.com>
2014-07-16 10:12:16 -0400

    Update timing to python 3

--- timing/profile.py
+++ timing/profile.py
@@ -1,16 +1,17 @@
-# Monary - Copyright 2011-2013 David J. C. Beach
+# Monary - Copyright 2011-2014 David J. C. Beach
 # Please see the included LICENSE.TXT and NOTICE.TXT for licensing information.
 
 from time import time
 
+
 class profile(object):
-    
+
     def __init__(self, name):
         self._name = name
-        
+
     def __enter__(self):
         self._start = time()
-        
+
     def __exit__(self, *args):
         stop = time()
-        print "%s took %6.2f s" % (self._name, stop - self._start)
+        print("%s took %6.2f s" % (self._name, stop - self._start))
--- timing/query_monary_block.py
+++ timing/query_monary_block.py
@@ -1,10 +1,11 @@
-# Monary - Copyright 2011-2013 David J. C. Beach
+# Monary - Copyright 2011-2014 David J. C. Beach
 # Please see the included LICENSE.TXT and NOTICE.TXT for licensing information.
 
-from monary import Monary
 import numpy
 
+from monary import Monary
 from profile import profile
+
 
 def do_monary_block_query():
     count = 0
@@ -12,18 +13,19 @@ def do_monary_block_query():
     with Monary("127.0.0.1") as m:
         with profile("monary block query"):
             for arrays in m.block_query(
-                "monary_test",                  # database name
-                "collection",                   # collection name
-                {},                             # query spec
-                ["x1", "x2", "x3", "x4", "x5"], # field names
-                ["float64"] * 5,                # field types
+                "monary_test",                   # database name
+                "collection",                    # collection name
+                {},                              # query spec
+                ["x1", "x2", "x3", "x4", "x5"],  # field names
+                ["float64"] * 5,                 # field types
                 block_size=32 * 1024,
             ):
                 count += len(arrays[0])
-                sums += [ numpy.sum(arr) for arr in arrays ]
+                sums += [numpy.sum(arr) for arr in arrays]
 
-    print "visited %i items" % count
-    print sums / count                          # prove that we did something...
+    print("visited %i items" % count)
+    # prove that we did something...
+    print(sums / count)
 
 if __name__ == '__main__':
     do_monary_block_query()
