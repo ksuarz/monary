ca7759aab52e94edd907fa512896a77c011a14f8
Kyle Suarez <ksuarz@gmail.com>
2014-07-21 15:06:27 -0400

    Add support for nested fields
    Use naive loops to load fields (nested fields included) from BSON into the NumPy
    arrays.

--- monary/cmonary.c
+++ monary/cmonary.c
@@ -625,10 +625,10 @@ int monary_bson_to_arrays(monary_column_data* coldata,
                           const bson_t* bson_data)
 {
     bson_iter_t bsonit;
-    const char* field;
+    bson_iter_t descendant;
     int i;
     int success;
-    int fields_found;
+    int masked;
     monary_column_item* citem;
 
     if (!coldata || !bson_data) {
@@ -636,46 +636,36 @@ int monary_bson_to_arrays(monary_column_data* coldata,
         return -1;
     }
     if (row > coldata->num_rows) {
-        DEBUG("Tried to load row %d, but that exceeds the maximum # of rows (%d)", row, coldata->num_rows);
-        return -1;
-    }
-    if (!bson_iter_init(&bsonit, bson_data)) {
-        DEBUG("%s", "Failed to initialize a BSON iterator");
+        DEBUG("Tried to load row %d but exceeded max rows (%d)", row, coldata->num_rows);
         return -1;
     }
 
-    // Mask all the values by default
+    masked = 0;
     for (i = 0; i < coldata->num_columns; i++) {
-        citem = coldata->columns + i;
-        if (citem->mask != NULL) {
-            citem->mask[row] = 1;
-        }
-    }
-
-    fields_found = 0;
-    while (bson_iter_next(&bsonit) && fields_found < coldata->num_columns) {
-        field = bson_iter_key(&bsonit);
         success = 0;
-
-        // Find the corresponding column
-        for (i = 0; i < coldata->num_columns; i++) {
-            citem = coldata->columns + i;
-
-            // Load the item, whose type should match the storage specified
-            if (strcmp(field, citem->field) == 0) {
+        citem = coldata->columns + i;
+        if (strstr(citem->field, ".")) {
+            // Look for a subdocument
+            bson_iter_init(&bsonit, bson_data);
+            if (bson_iter_find_descendant(&bsonit, citem->field, &descendant)) {
+                success = monary_load_item(&descendant, citem, row);
+            }
+        } else {
+            // Look for a top-level field
+            if (bson_iter_init_find(&bsonit, bson_data, citem->field)) {
                 success = monary_load_item(&bsonit, citem, row);
-                ++fields_found;
-
-                // Record success in the mask, if applicable
-                if (citem->mask != NULL) {
-                    citem->mask[row] = !success;
-                }
-                break;
             }
         }
+
+        if (citem->mask != NULL) {
+            citem->mask[row] = !success;
+        }
+        if (!success) {
+            masked++;
+        }
     }
 
-    return coldata->num_columns - fields_found;
+    return masked;
 }
 
 /**
